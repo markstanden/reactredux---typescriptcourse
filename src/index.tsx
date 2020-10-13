bc400d4a940f1d0b7cafc003cdcb566834c93577
Mark Standen <mark@markstanden.uk>
2020-10-13 22:33:04 +0100

    added state to app

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,27 @@ interface AppProps {
 }
 
 class App extends React.Component<AppProps> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = { counter: 0 };
+  }
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
   render() {
-    return <div>{this.props.color} Hello World </div>;
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
   }
 }
 
