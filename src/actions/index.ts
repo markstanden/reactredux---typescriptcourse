cc6de8e97edcda17537dc4cbaebe330f7b8658dc
Mark Standen <mark@markstanden.uk>
2020-10-14 21:00:16 +0100

    added ToDoObject Interface

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,14 +1,24 @@
 import axios from "axios";
 import { Dispatch } from "redux";
+import { ActionTypes } from "./types";
 
 const url = "https://jsonplaceholder.typicode.com/todos";
 
+interface ToDoObject {
+  userId?: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+
+
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
-    const response = await axios.get(url);
+    const response = await axios.get<ToDoObject[]>(url);
 
     dispatch({
-      type: "FETCH_TODOS",
+      type: ActionTypes.fetchTodos,
       payload: response.data,
     });
   };
